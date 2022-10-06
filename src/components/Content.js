3ec53b9de8e03947a185bb2d91f7162c0ad43162
alexdean2112 <alexdeanofficial2112@gmail.com>
2022-10-06 12:53:51 +0100

    fetchCatData in useEffect in Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,23 +1,28 @@
-import styled from 'styled-components'
-import Cat from './Cat'
-
-
+import Cat from "./Cat"
+import { useState, useEffect } from "react"
 
 const Content = () => {
+const [counter, setCounter] = useState(0)
+const [data, setData] = useState()  
+
+
+useEffect(() => {
+    async function fetchCatData() {
+        const response = await fetch("https://api.thecatapi.com/v1/breeds/?api_key=live_Tt7xdd37BnW9MoqD2zlLy1dg8C9qPlKLdxNXGoNylalEBw39TaM7CbOYfkfhdSo1")
+        const data = await response.json()
+        console.log(data)
+    } 
+    fetchCatData()
+},[])
+
     return (
-        <Container>
-           <Cat />
-            <Cat/>
-            <Cat/>
-
-
-
-        </Container>
+        <div>
+            {/* <Cat name={data[0 + (counter)].name}/>
+            <Cat name={data[0 + (counter + 1)].name}/>
+            <Cat name={data[0 + (counter + 2)].name}/> */}
+            <button>More Cats!</button>
+        </div>
     )
 }
 
 export default Content;
-
-const Container = styled.div`
-    background-color:#DBEFE1;
-`
