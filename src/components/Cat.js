e67b9b56f083fea29c9fab7d5bae4043d8427746
JRobbo85 <jrobertson85@hotmail.co.uk>
2022-10-06 16:37:40 +0100

    Fixing desync between local repo and git repo

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,21 +1,21 @@
 import styled from 'styled-components' 
+import {faker} from '@faker-js/faker'
 
-
-const Cat = ({name}) => {
-
-
+const Cat = ({entireCat}) => {
+const value = faker.commerce.price(50, 100, 2, '£')
 
     return (
         <Container>
+            {/* <Image src ={entireCat.image.url} /> */}
             <Image src ={require('./assets/Cats4Lyf_logo.png')} />
             <Cat_Details>
-                <p>Breed: {name}</p>
-                <p>Temperament:</p>
-                <p>Origin: </p>
+                <p>Breed: {entireCat.name}</p>
+                <p>Lifespan: {entireCat.life_span} years</p>
+                <p>Origin: {entireCat.origin}</p>
             </ Cat_Details>
             <Price_Details>
-                <p>Price:</p>
-                <button>Add To cart</button>
+                <p>Price: {value}</p>
+                <Button>Add To cart</Button>
             </Price_Details>
         </Container>
     )
@@ -65,8 +65,13 @@ font-family: 'Poppins', sans-serif;
 flex-direction:column;
 margin-left:1vw;
 margin-top:7vh;
-border-style:solid;
-border-weight:2px;
-border-color:green;
+
+`
+
+const Button = styled.button `
+    background-color:transparent;
+    border: 2px solid black;
+    padding: 1vw;
+    
 `
 
