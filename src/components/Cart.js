783387a4e037d8a346bba2d327960d94574d0593
Alia <106494326+aliavarzea@users.noreply.github.com>
2022-10-07 16:27:23 +0100

    changes to cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,40 +1,40 @@
-import {Faker} from "@faker-js/faker"
-import styled from 'styled-components' 
-import {useState} from 'react'
+import { Faker } from "@faker-js/faker"
+import styled from 'styled-components'
+import { useState } from 'react'
 
 
-const Cart = ({basket}) =>{
+const Cart = ({ basket }) => {
 
-const values = []
+    const values = []
 
 
-const addUp = () => {
-    let sum = 0
-    for (let i = 0; i < values.length; i++){
-    sum += values[i]
+    const addUp = () => {
+        let sum = 0
+        for (let i = 0; i < values.length; i++) {
+            sum += values[i]
+        }
+        return sum
     }
-return sum 
-}
     return (
-        <div id="cartTab">
+        <CartTab>
             <p>Cart</p>
-            <div id="catInBskt">
-            {basket.map((item, index) => {
-                values.push(parseInt(item.price))
-                return (
-            <div>
-                
-            <Image src = {item.image.url} />
-            <p>{item.name}</p>
-            <p>£{item.price}</p>
-            </div>)
-})}
-            </div>
-            <div id="totalCost">
+            <CatInBasket>
+                {basket.map((item, index) => {
+                    values.push(parseInt(item.price))
+                    return (
+                        <div>
+
+                            <Image src={item.image.url} />
+                            <p>{item.name}</p>
+                            <p>£{item.price}</p>
+                        </div>)
+                })}
+            </CatInBasket>
+            <TotalCost>
                 <p>Total: £{addUp()}.00</p>
-            </div>
-            <button id="checkout">Checkout</button>
-        </div>
+            </TotalCost>
+            <Checkout>Checkout</Checkout>
+        </CartTab>
     )
 }
 
@@ -42,6 +42,23 @@ export default Cart
 
 
 
-const Image = styled.img `
-    width: 4vw;
+const CartTab = () => styled.div`
+border: 1px solid red;
+`
+
+const CatInBasket = styled.div`
+border: 1px solid yellow;
+`
+
+const Image = styled.image`
+width: 4vw;
+`
+
+const TotalCost = styled.div`
+border: 1px solid blue;
+
+`
+const Checkout = styled.button`
+border: 1px solid green;
+
 `
