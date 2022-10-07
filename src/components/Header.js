06b129ae0104ba67d34836b3bbcb48bd4d7a8d36
Alia <106494326+aliavarzea@users.noreply.github.com>
2022-10-07 11:19:19 +0100

    css changes to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,26 @@
+import styled from 'styled-components'
+
+
 const Header = () => {
   return (
-    <div>
+    <Headerwrap>
       <img
         src={require("./images/Cats4Lyf_logo.png")}
         alt="Cat logo"
         id="catLogo"
       ></img>
-      <h1> Cats 4 Lyfe </h1>
-    </div>
+      <h1>Cats</h1>
+      <h1>4</h1>
+      <h1>Lyfe </h1>
+    </Headerwrap>
   );
 };
 
 export default Header;
+
+// CSS for Header component
+
+const Headerwrap = styled.div`
+width: 10%;
+background: #181133;
+`;
